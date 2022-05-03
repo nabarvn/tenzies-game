1a15971909791724ae5fa54f6c8c9677906bb1c5
Nabarun <nicknabarun@gmail.com>
2022-05-03 19:30:58 +0530

    Updates

---
+++ precache-manifest.9541c9a6e88efe444bb84af57c2f6799.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d0465b1257306725af1",
+    "url": "/tenzies-game/static/css/main.47c40869.chunk.css"
+  },
+  {
+    "revision": "9d0465b1257306725af1",
+    "url": "/tenzies-game/static/js/main.9d0465b1.chunk.js"
+  },
+  {
+    "revision": "668d3b95e11bdd1e5334",
+    "url": "/tenzies-game/static/js/1.668d3b95.chunk.js"
+  },
+  {
+    "revision": "1d2148d14866088b7247",
+    "url": "/tenzies-game/static/js/runtime~main.1d2148d1.js"
+  },
+  {
+    "revision": "fd39008d882159c595a15746db57d881",
+    "url": "/tenzies-game/index.html"
+  }
+];
